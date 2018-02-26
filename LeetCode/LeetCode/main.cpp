fa185f5a175e16a1262c9d593ff203c34248dc05
彦子凡 <candycookieyk@gmail.com>
2018-02-26 22:09:07 +0800

    3.删除测试文件夹，提交相关代码

---
+++ LeetCode/LeetCode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LeetCode
+//
+//  Created by 彦子凡 on 2018/2/26.
+//  Copyright © 2018年 彦子凡. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
