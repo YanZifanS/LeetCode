d0505d0eca03fdba48c94175f9d387408ee3b13a
YanZifan <candycookieyk@gmail.com>
2018-03-07 16:17:44 +0800

    28题的解答

--- Test/Test/Test/Test.cpp
+++ Test/Test/Test/Test.cpp
@@ -2,10 +2,96 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+
+using namespace std;  
+
+//struct ListNode 
+//{
+//     int val;
+//     ListNode *next;
+//     ListNode(int x) : val(x), next(NULL) {}
+//};
+//
+//ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) 
+//{
+//	//定义头结点
+//	ListNode *head;
+//	while()
+//	{
+//		
+//	}
+//	return pNode;
+//}
+
+int lengthOfLongestSubstring(string s) 
+{
+	int nResult = -1;
+	if (!s.empty())
+	{
+
+	}
+	return nResult;
+}
+
+//28
+int strStr(string haystack, string needle)
+{
+	if ((haystack.empty() && !needle.empty()) || (needle.empty() && !haystack.empty()))
+	{
+		return -1;
+	}
+	else if (haystack.empty() && needle.empty())
+	{
+		return 0;
+	}
+
+	if (haystack.length() < needle.length())
+	{
+		return -1;
+	}
+	else if (haystack.length() == needle.length())
+	{
+		if (haystack.compare(needle) != 0)
+		{
+			return -1;
+		}
+		return 0;
+	}
+	else
+	{
+		int nLength = haystack.length();
+		for (int i=0; i<nLength; i++)
+		{
+			int nPos = haystack.find(needle[0]);
+			if (nPos < 0)
+			{
+				return -1;
+			}
+			//ispsiss1ippi
+			haystack = haystack.substr(nPos, nLength-nPos);
+			string strHar = haystack;
+			if (haystack.substr(0, needle.length()).compare(needle) == 0)
+			{
+				return nLength-haystack.length();
+			}
+			haystack = strHar.substr(1, strHar.length()-1);
+		}
+		return -1;
+	}
+	return -1;
+}
+
+//58
+int lengthOfLastWord(string s)
+{
+
+}
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	int nPos = strStr("mispsiss1ippi", "pi");
 	return 0;
 }
 
